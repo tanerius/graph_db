3518b791c501bfc032f416bdce4171420e3197fb
Tanerius <tanerius@gmail.com>
2016-02-28 15:10:29 +0100

    Process implementation completed

--- graph/bobprocess.c
+++ graph/bobprocess.c
@@ -1,2 +1,32 @@
 #include "bobprocess.h"
 
+pid_t fork_child(void (*func)(void* arg), Shared_msg_t* state) {
+    pid_t child_pid;
+    switch (child_pid = fork()) {
+        case ERROR:
+            // could not fork
+            return ERROR;
+            break;
+        case 0:
+            // successfully forked this is the child 
+            (*func)((Shared_msg_t*)state); 
+            return 0; 
+            break;
+        default: 
+            // parent continues
+            return child_pid;
+            break;
+    }
+}
+
+void wait_for_children(pid_t* children, const unsigned char processes) {
+    int status;
+    while (ERROR < wait(&status)) {
+        if (!WIFEXITED(status)) {
+            for (int c = 0; c < processes; c++) {
+                kill(children[c], SIGKILL);
+            }
+            break;
+        }
+    }
+}
--- graph/bobprocess.h
+++ graph/bobprocess.h
@@ -5,8 +5,18 @@
     These are helpers for process control.
 */
 #ifndef _BOBUTILS_H_
-#include "bobutils.h"
+    #include "bobutils.h"
 #endif
 
+#ifndef _BOBIPC_H_
+    #include "bobipc.h"
+#endif
+#include <signal.h>
+
+/*
+    a function which helps to fork children processes by providind a handler function as an argument and an IPC variable
+*/
+pid_t fork_child(void (*func)(void*), Shared_msg_t*); 
+void wait_for_children(pid_t*, const unsigned char); // function to instruct parent to wait for its children
 
 #endif
