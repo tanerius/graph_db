e4051eaa04803af53525485b50e7e022292a414c
Tanerius <tanerius@gmail.com>
2016-05-20 18:20:56 +0200

    test file for all other things

---
+++ graph/tests/test_other.cpp
@@ -0,0 +1,19 @@
+#ifndef _PRIMITIVES_H_
+#include "primitives.h"
+#endif
+#include <cstring>          /* for strlen, strcpy and memset */
+#include <cstdlib>          /* for itoa */
+#include <climits>
+#include <assert.h>
+#include <sstream>          /* For to_string */
+
+
+int main(){
+    printf("TESTING RANDOM STUFF \n");
+    if(nullptr){
+        printf("nullptr evaluates true\n");
+    }
+    else{
+        printf("nullptr evaluates false\n");
+    }
+}
