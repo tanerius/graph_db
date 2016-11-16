15ec635495fabbe8799b381b34565f979a5c0894
Taner Selim <tanerius@gmail.com>
2016-11-16 22:02:53 +0100

    Started graph

--- graph/graphutils.hpp
+++ graph/graphutils.hpp
@@ -39,178 +39,180 @@
 namespace GDBUtils
 {
 
-/* Helper macros */
-#define minElement(a,b)            ((a)<(b)?(a):(b))
-#define maxElement(a,b)            ((a)>(b)?(a):(b))
-#define safeDelete(_x)      { if (_x) { delete (_x); (_x) = nullptr; } }
-#define safeDeleteArray(_x) { if (_x) { delete [] (_x); (_x) = nullptr; } }
-#define safeRelease(_x)     { if (_x) { (_x)->Release(); (_x) = nullptr; } }
+    /* Helper macros */
+    #define minElement(a,b)            ((a)<(b)?(a):(b))
+    #define maxElement(a,b)            ((a)>(b)?(a):(b))
+    #define safeDelete(_x)      { if (_x) { delete (_x); (_x) = nullptr; } }
+    #define safeDeleteArray(_x) { if (_x) { delete [] (_x); (_x) = nullptr; } }
+    #define safeRelease(_x)     { if (_x) { (_x)->Release(); (_x) = nullptr; } }
 
 
-/*
-    A simple function to check endianness
-*/
-inline bool isBigEndian(){
-    return htonl(47) == 47;
-}
+    /*
+        A simple function to check endianness
+    */
+    inline bool isBigEndian(){
+        return htonl(47) == 47;
+    }
 
-/*
-    A simple function to return a string timestamp
-*/
-inline const char* getTimestamp()
-{
-    time_t now = time (NULL);
-    char* date = ctime(&now);
-    return date;
-}
+    /*
+        A simple function to return a string timestamp
+    */
+    inline const char* getTimestamp()
+    {
+        time_t now = time (NULL);
+        char* date = ctime(&now);
+        return date;
+    }
 
 
 
-/*
-    A general class from which almost all Gdb all classes must inherit to use advanced functionalities. 
+    /*
+        A general class from which almost all Gdb all classes must inherit to use advanced functionalities. 
 
-    Things to consider to add here:
-    - version number
-*/
-class GdbObject /* wish it wont sound like Java */
-{
-    public:
-        virtual const char* objDisplay() = 0;
-        virtual const char* objGetID() = 0;
+        Things to consider to add here:
+        - version number
+    */
+    class GdbObject /* wish it wont sound like Java */
+    {
+        public:
+            virtual const char* objDisplay() = 0;
+            virtual const char* objGetID() = 0;
 
-        inline void timer_start() 
-        {
-            event_start = std::chrono::high_resolution_clock::now();
-        }
-
-        inline void timer_stop()
-        {
-            event_end = std::chrono::high_resolution_clock::now();
-            event_diff = event_end - event_start;
-        }
-
-        inline GdbVariant get_timer() 
-        {
-            return event_diff.count(); 
-        }
-        
-        // Throw in basic logging
-        static bool restart_log()
-        {
-            FILE* file = fopen(GDB_LOCAL_LOG_FILE, "w");
-            if (!file) 
+            inline void timer_start() 
             {
-                fprintf(
-                    stderr,
-                    "ERROR: Could not open GDB_LOCAL_LOG_FILE log file %s for writing\n",
-                    GDB_LOCAL_LOG_FILE
-                );
-                return false;
+                event_start = std::chrono::high_resolution_clock::now();
             }
-            time_t now = time (NULL);
-            char* date = ctime(&now);
-            fprintf(file, "GDB_LOCAL_LOG_FILE log. Local time %s \n", date);
-            fclose(file);
-            return true;
-        }
-        //bool gl_log(const char* message, ...);
-        /* add a message to the log file. arguments work the same way as printf() */
-        static bool log (const char* message, ...) 
-        {
-            va_list argptr;
-            FILE* file = fopen (GDB_LOCAL_LOG_FILE, "a");
-            if (!file) {
-                fprintf (
-                    stderr,
-                    "ERROR: could not open GDB_LOCAL_LOG_FILE %s file for appending\n",
-                    GDB_LOCAL_LOG_FILE
-                );
-                return false;
+
+            inline void timer_stop()
+            {
+                event_end = std::chrono::high_resolution_clock::now();
+                event_diff = event_end - event_start;
             }
-            va_start (argptr, message);
-            vfprintf (file, message, argptr);
-            va_end (argptr);
-            fclose (file);
-            return true;
-        }
 
-        static bool gl_log_err (const char* message, ...) 
-        {
-            va_list argptr;
-            FILE* file = fopen (GDB_LOCAL_LOG_FILE, "a");
-            if (!file) {
-                fprintf (
-                    stderr,
-                    "ERROR: could not open GDB_LOCAL_LOG_FILE %s file for appending\n",
-                    GDB_LOCAL_LOG_FILE
-                );
-                return false;
+            inline GdbVariant get_timer() 
+            {
+                return event_diff.count(); 
             }
-            va_start (argptr, message);
-            vfprintf (file, message, argptr);
-            va_end (argptr);
-            va_start (argptr, message);
-            vfprintf (stderr, message, argptr);
-            va_end (argptr);
-            fclose (file);
-            return true;
-        }
-    private:
-        // Make some stuff for being able to measure time
-        std::chrono::time_point<std::chrono::high_resolution_clock> event_start;
-        std::chrono::time_point<std::chrono::high_resolution_clock> event_end;
-        std::chrono::duration<double> event_diff;
-};
+            
+            // Throw in basic logging
+            static bool restart_log()
+            {
+                FILE* file = fopen(GDB_LOCAL_LOG_FILE, "w");
+                if (!file) 
+                {
+                    fprintf(
+                        stderr,
+                        "ERROR: Could not open GDB_LOCAL_LOG_FILE log file %s for writing\n",
+                        GDB_LOCAL_LOG_FILE
+                    );
+                    return false;
+                }
+                time_t now = time (NULL);
+                char* date = ctime(&now);
+                fprintf(file, "GDB_LOCAL_LOG_FILE log. Local time %s \n", date);
+                fclose(file);
+                return true;
+            }
+            //bool gl_log(const char* message, ...);
+            /* add a message to the log file. arguments work the same way as printf() */
+            static bool log (const char* message, ...) 
+            {
+                va_list argptr;
+                FILE* file = fopen (GDB_LOCAL_LOG_FILE, "a");
+                if (!file) {
+                    fprintf (
+                        stderr,
+                        "ERROR: could not open GDB_LOCAL_LOG_FILE %s file for appending\n",
+                        GDB_LOCAL_LOG_FILE
+                    );
+                    return false;
+                }
+                va_start (argptr, message);
+                vfprintf (file, message, argptr);
+                va_end (argptr);
+                fclose (file);
+                return true;
+            }
+
+            static bool gl_log_err (const char* message, ...) 
+            {
+                va_list argptr;
+                FILE* file = fopen (GDB_LOCAL_LOG_FILE, "a");
+                if (!file) {
+                    fprintf (
+                        stderr,
+                        "ERROR: could not open GDB_LOCAL_LOG_FILE %s file for appending\n",
+                        GDB_LOCAL_LOG_FILE
+                    );
+                    return false;
+                }
+                va_start (argptr, message);
+                vfprintf (file, message, argptr);
+                va_end (argptr);
+                va_start (argptr, message);
+                vfprintf (stderr, message, argptr);
+                va_end (argptr);
+                fclose (file);
+                return true;
+            }
+        private:
+            // Make some stuff for being able to measure time
+            std::chrono::time_point<std::chrono::high_resolution_clock> event_start;
+            std::chrono::time_point<std::chrono::high_resolution_clock> event_end;
+            std::chrono::duration<double> event_diff;
+    };
 
 
 
-class GdbTester : public GdbObject 
-{
-    public:
-        const char* objDisplay() 
-        {
-            return "\nobjDisplay() method called...\n";
-        }
+    class GdbTester : public GdbObject 
+    {
+        public:
+            const char* objDisplay() 
+            {
+                return "\nobjDisplay() method called...\n";
+            }
 
-        const char* objGetID()
-        {
-            return "\nobjGetID() method called...\n";
-        }
-};
+            const char* objGetID()
+            {
+                return "\nobjGetID() method called...\n";
+            }
+    };
 
-/*
-    GdbBinFile thread safe implementation of a binary file manipulation class. Can be used generically for binary files.
-*/
-class GdbBinFile : public GdbObject
-{    
-    private:
-        FILE* m_pFile;
-        GdbString m_fileName;
+    /*
+        GdbBinFile thread safe implementation of a binary file manipulation class. Can be used generically for binary files.
+    */
+    class GdbBinFile : public GdbObject
+    {    
+        private:
+            FILE* m_pFile;
+            GdbString m_fileName;
 
-        bool checkOpen()
-        {
-            return !((m_pFile == NULL) || (m_pFile == nullptr));
-        }
+            bool checkOpen()
+            {
+                return !((m_pFile == NULL) || (m_pFile == nullptr));
+            }
 
-    public: // properties
-        std::mutex file_mutex;
-        
-    public: // member funcitons
-        GdbBinFile(const char* fileName)
-        {
-            m_pFile = nullptr;
-            m_fileName = fileName;
-        }
-                
-        const char* objGetID() 
-        { 
-            return m_fileName.cstr(); 
-        }
+        public: // properties
+            std::mutex file_mutex;
+            
+        public: // member funcitons
+            GdbBinFile(const char* fileName)
+            {
+                m_pFile = nullptr;
+                m_fileName = fileName;
+            }
+                    
+            const char* objGetID() 
+            { 
+                return m_fileName.cstr(); 
+            }
 
-        bool setFile(const char* newFile);    
-};
-
+            bool setFile(const char* newFile);    
+    };
+    
 }
 
 
 #endif
+
+
