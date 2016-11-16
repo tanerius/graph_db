7c2b26d1a6e61c6f2a9cb5377be1a75064a064e8
Taner Selim <tanerius@gmail.com>
2016-11-17 00:02:55 +0100

    boost graph

--- graph/graph.hpp
+++ graph/graph.hpp
@@ -10,6 +10,11 @@
 #include "../graphutils.hpp"
 #endif
 
+#include <boost/graph/adjacency_list.hpp>
+
+typedef boost::adjacency_list<> GdbGraphTypeDirected;
+typedef boost::adjacency_list<boost::vecS,boost::vecS,boost::undirectedS> GdbGraphTypeUndirected;
+
 /*
     The interface for GDB defining all the primary graph operations
     V -> Vertex Type Representation
@@ -18,47 +23,54 @@
 template < typename U, typename V >
 class GdbGraph : public GdbUtils::GdbObject
 {
-    protected:
-        bool m_is_directional;
+    private:
+        bool m_is_directed;
+
+        GdbGraphTypeDirected m_graph;
+
         Gdb_N_t m_vertexes;
         Gdb_N_t m_edges;
 
     public:
         // Ctor only for direct initialization
-        explicit IGdb(bool _isDirectional, Gdb_N_t _edges, Gdb_N_t _vertexes)
-        : m_is_directional(_isDirectional)
+        explicit GdbGraph(bool _isDirected, Gdb_N_t _edges, Gdb_N_t _vertexes)
+        : m_is_directed(_isDirected)
         , m_edges( _edges )
         , m_vertexes( _vertexes ) {}
 
-        virtual ~IGdb() {};
         /*
             Add edge between _v1 and _v2 vertexes.
         */
-        virtual void AddEdge(const V &_v1, const V &_v2)=0;
+        void            AddEdge(const V &_v1, const V &_v2);
         /*
             Add vertex _v to the graph
         */
-        virtual void AddVertex(V &_v)=0;
+        void            AddVertex(V &_v);
         /*
             Tests whether there is an edge from _v1 to _v2;
         */
-        virtual bool Adjacent(const V &_v1, const V &_v2) = 0;
+        bool            Adjacent(const V &_v1, const V &_v2);
 
         //virtual GetEdgeValue(const V &_v1, GdbString &_prop)=0;
         //virtual V& GetVertexValue(GdbString &_id)=0;
 
+        GdbGraphType    create_empty_directed_graph()
+        {
+            return {};
+        }
+
         /*
             Gets a list of all vertexes that have an edge to _x
         */
-        virtual V* Neighbours (Gdb_N_t &_size, const V& _x) = 0;
+        V*              Neighbours (Gdb_N_t &_size, const V& _x);
         /*
             Remove edge between _v1 and _v2
         */
-        virtual void RemoveEdge(const V &_v1, const V &_v2)=0;
+        void            RemoveEdge(const V &_v1, const V &_v2);
         /*
             Remove vertex _v from the graph (also removing edges)
         */
-        virtual void RemoveVertex(V &_v)=0;
+        void            RemoveVertex(V &_v);
 
         //virtual void SetEdgeValue(GdbString &_prop)=0;
         //virtual void SetVertexValue(GdbString &_prop)=0;
